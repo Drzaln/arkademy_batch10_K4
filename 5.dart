1ea4c0a88e7dd8aa08f75f8ec2c92650d96bc76d
Drzaln <drzaln@null.net>
2019-05-18 18:45:12 +0700

    update lagi

--- 5.dart
+++ 5.dart
@@ -1,9 +1,16 @@
+import 'dart:math';
+
 var data = ['h','g','a','b','d','f'];
 var dataLain = ['a','b','c','d'];
 
 minmax(data){
   for(var i=0; i<data.length; i++){
-    
+    var minus = data.reduce(min);
+    var maxus = data.reduce(max);
+
+    if(maxus < minus){
+      data.reduce(max(a));
+    }
   }
 }
 
