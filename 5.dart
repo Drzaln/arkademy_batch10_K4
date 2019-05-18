8511fe1bfdc09f30fe2ba2d75393f83361718ba5
Drzaln <drzaln@null.net>
2019-05-18 20:31:38 +0700

    update lagi nomer 5

--- 5.dart
+++ 5.dart
@@ -1,19 +1,14 @@
-import 'dart:math';
-
 var data = ['h','g','a','b','d','f'];
 var dataLain = ['a','b','c','d'];
 
 minmax(data){
-  for(var i=0; i<data.length; i++){
-    var minus = data.reduce(min);
-    var maxus = data.reduce(max);
-
-    if(maxus < minus){
-      data.reduce(max(a));
-    }
+  var datum = data.length;
+  for(var i=0;i<datum;i++){
+    data.sort();
   }
+  return print(data);
 }
 
 main(){
-  minmax(dataLain);
+  minmax(data);
 }
